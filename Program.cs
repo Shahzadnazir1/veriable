f011c69d1c2f34912da5d52e1d8fded121c33933
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-04-16 23:31:32 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace consoleApp10
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int width = 10;
+            int height = 30;
+            Console.WriteLine("Area is :" + (height * width));
+            if (height > 20) 
+            {
+                Console.WriteLine("30 is larger than thershold ");
+            }
+        }
+    }
+}
+
