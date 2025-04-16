be81393f35bd20300817fb4a92de33a5f5dc2eff
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-04-17 00:00:22 +0500

    Add files via upload

---
+++ Program ..cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+namespace consoleApp10
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string studentName;   //declear the veriable 
+            int  samester;
+            double cgpa;
+            //pass the value to the veriable
+            studentName = "Shahzad";
+            samester = 3;
+            cgpa = 2.6;
+            //print the value of veriable
+            Console.WriteLine(" studentName" + studentName);
+            Console.WriteLine("samester" + samester);
+            Console.WriteLine("cgpa" + cgpa);
+
+
+
+
+        }
+    }
+}
+
